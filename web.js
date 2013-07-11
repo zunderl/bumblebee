50c9c90c27967eff633112c3aed83fb5870ca3b0
Zunder Lekshmanan <zunder.lekshmanan@gmail.com>
2013-07-11 13:13:07 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs=require('fs);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+var data ;
+try {
+	data = fs.readFileSync('index.html') ;
+} catch (e){
+	if (e instanceof Error)
+	{
+		if (e.code == 'ENOENT')
+		{
+		  response.send('File not found') ;
+			
+		}
+		else
+		{
+			throw e;
+		}
+	}
+
+}
+response.send(data.toString());
+
 });
 
 var port = process.env.PORT || 5000;
